b38062f034f5850a81e3025618d029fae732ea18
Glen Jolley <novaxeros@gmail.com>
2018-02-12 18:02:16 +0000

    Initial Commit

--- js/carriers.js
+++ js/carriers.js
@@ -1,8 +1,8 @@
 var carriers_arr = {
 
-"amber":[{"ID":1,"Name":"Please select"}],
+"amber":[{"ID":1,"Name":"Please select"},{"ID":2,"Name":"None"}],
 
-"twg":[{"ID":1,"Name":"Please select"}]
+"twg":[{"ID":1,"Name":"Please select"},{"ID":2,"Name":"None"}]
 
 };
 
